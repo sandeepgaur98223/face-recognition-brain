45ec60a1b06ccb753f8b82215301d83e7897f87f
sandeepgaur98223 <sandeepgaur98223@gmail.com>
2023-03-25 14:55:49 +0530

    working static signin and register with the app

--- src/App.js
+++ src/App.js
@@ -33,9 +33,40 @@ constructor(){
     imageUrl:'',
     box:{},
     route:'SignIn',
-    isSignedIn:false
+    isSignedIn:false,
+    user:  {
+      id:'',
+      name:'',
+      email:'',
+      entries:0,
+      joined: ''
+
+  }
   }
 }
+
+loadUser=(data)=>{
+
+this.setState({user : {
+  id: data.id,
+  name:data.name,
+  email:data.email,
+  entries:data.entries,
+  joined: data.joined
+}})
+
+}
+
+
+/*
+componentDidMount(){
+  fetch('http://localhost:3001/')
+  .then(response=>response.json())
+ // .then(console.log());
+ 
+  .then(data=>console.log('1',data))
+}
+*/
 
 onInputChange=(event)=>{
   console.log(event.target.value);
@@ -135,7 +166,7 @@ render(){
       ? <SignIn onRouteChange={this.onRouteChange}/>
       : (
         this.state.route==='Register'
-        ?<Register onRouteChange={this.onRouteChange}/>
+        ?<Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
         :<div>
           <Logo />
           <Rank />
